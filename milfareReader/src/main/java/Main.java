da98257d83c28983907df05f655bd371e8eb0fdf
sandy <sandytang2406@gmail.com>
2021-12-16 19:34:19 +0100

    implemented Enquiry card protocol

---
+++ milfareReader/src/main/java/Main.java
@@ -0,0 +1,49 @@
+import at.ac.htl.leonding.mifare.MifareReader;
+import com.fazecast.jSerialComm.SerialPort;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class Main {
+    public static void main(String[] args) {
+        findReader();
+
+    }
+
+    private static void findReader() {
+        var reader = MifareReader.findReader();
+        System.out.println(reader.toString());
+        reader.open();
+        reader.beep();
+        reader.close();
+
+        reader.open();
+        reader.enquiryCard();
+        reader.close();
+    }
+
+/*    byte[] prot ={
+        "enquiry_module": "\x03\x12\x00\x15",
+                'enquiry_module_return': '\x02\x12\x14',
+                'active_buzzer': '\x02\x13\x15',
+                'enquiry_card': pack('BBBB', 03, 02, 00, 05),
+                'enquiry_cards_return': '\x03\x02\x01\x06',
+        'enquiry_no_card_found': '\x02\x01\x03',
+        'enquiry_all_cards': '\x03\x02\x01\x05',
+                'anticollision' : '\x02\x03\x05\x00',
+                'select_card' : '\x02\x04\x06"}*/
+
+
+
+    public static boolean portIsOpen(SerialPort comPort) {
+        var reader = new MifareReader(comPort);
+        reader.open();
+        var isOpen = reader.hello();
+        System.out.println(isOpen? "open\n" : "closed\n");
+        reader.close();
+        return isOpen;
+    }
+
+}
---
+++ milfareReader/src/main/java/at/ac/htl/leonding/mifare/MifareReader.java
@@ -0,0 +1,144 @@
+package at.ac.htl.leonding.mifare;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortIOException;
+import com.fazecast.jSerialComm.SerialPortTimeoutException;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+
+/**
+ * interface for M302 Mifare Cardreader.
+ */
+public class MifareReader {
+
+    public OutputStream os;
+
+    /**
+     * serial port where the reader is connected to.
+     */
+    final private SerialPort port;
+
+    public MifareReader(SerialPort port) {
+        this.port = port;
+    }
+
+    /**
+     * send enquiry if module exist to the reader.
+     *
+     * @return true if reader is present
+     */
+    public boolean hello() {
+        var ok = false;
+        System.out.println("checking" + port.getSystemPortName());
+
+        os = port.getOutputStream();
+        var greeting = new byte[]{0x03, 0x12, 0x00, 0x15};
+        var response = new byte[]{0x02, 0x12, 0x14};
+
+        ok = communicateWithReader(os,greeting,response);
+
+        return ok;
+    }
+
+
+
+    public static MifareReader findReader() {
+        SerialPort openPort = null;
+        var comPorts = SerialPort.getCommPorts();
+         var serialPort = Arrays.stream(comPorts).filter(port -> portIsOpen(port)).findFirst().orElse(null);
+         return serialPort != null ? new MifareReader(serialPort) : null;
+    }
+
+    public static boolean portIsOpen(SerialPort comPort) {
+        var reader = new MifareReader(comPort);
+        reader.open();
+        var isOpen = reader.hello();
+        System.out.println(isOpen? "open" : "closed");
+        reader.close();
+        return isOpen;
+    }
+
+    public boolean beep() {
+         os = port.getOutputStream();
+        var ok = false;
+
+        var greeting = new byte[]{0x02, 0x13, 0x15};
+        var response = greeting;
+
+        ok = communicateWithReader(os,greeting,response);
+        return ok;
+    }
+
+    public boolean enquiryCard() {
+        os = port.getOutputStream();
+        var ok = false;
+
+        var greeting = new byte[]{0x03, 0x02, 0x00, 0x05};
+        var response = new byte[]{0x03, 0x02, 0x01, 0x06};
+
+        ok = communicateWithReader(os,greeting,response);
+        return ok;
+    }
+
+    public boolean anticollision() {
+/*        os = port.getOutputStream();
+        var ok = false;
+
+        var greeting = new byte[]{0x02, 0x03, 0x05};
+        var response = new byte[]{0x06, 0x03, 0x32, 0x78, 0x13, 0x0E8,  0x0AE};
+
+        ok = communicateWithReader(os,greeting,response);
+        return ok;*/
+    }
+
+    public boolean communicateWithReader(OutputStream os, byte[] greeting, byte[] response){
+        var ok = false;
+        byte[] buffer ;
+        try {
+            os.write(greeting);
+            os.flush();
+
+            InputStream in = port.getInputStream();
+
+            if(response.length == 3){
+                 buffer = new byte[3];
+            }else {
+                buffer = new byte[4];
+            }
+
+            for (int j = 0; j < buffer.length; ++j) {
+                byte ch = (byte) in.read();
+                buffer[j] = ch;
+                var hex = Integer.toHexString(ch);
+                System.out.print(hex);
+            }
+
+            if (Arrays.equals(buffer, response)) {
+                ok = true;
+            }
+            System.out.println();
+            in.close();
+        } catch (SerialPortTimeoutException ex) {
+            System.out.println("not found");
+        }catch(SerialPortIOException e){
+            System.out.println("failed");
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+        return ok;
+    }
+
+    public void open() {
+        port.openPort();
+        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 250, 250);
+    }
+
+    public void close() {
+        port.closePort();
+    }
+
+
+}
