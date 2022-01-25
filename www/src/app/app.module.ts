8af498fcfe2a7a607ae983609aff9f5bcd5cb619
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2022-01-25 09:53:21 +0100

    add service and view

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MealsOverviewComponent } from './view/meals-overview/meals-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MealsOverviewComponent
   ],
   imports: [
     BrowserModule
