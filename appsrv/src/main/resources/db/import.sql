f48156054783483a01269e3a7b1fc50282e6745e
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2022-01-28 09:00:05 +0100

    insert into m_consumation table

--- appsrv/src/main/resources/db/import.sql
+++ appsrv/src/main/resources/db/import.sql
@@ -3,3 +3,13 @@ insert into m_person (first_name, last_name, entry_year) values ('Willow','Rowe'
 insert into m_person (first_name, last_name, entry_year) values ('Glenna','Britt',2014);
 insert into m_person (first_name, last_name, entry_year) values ('Keiko','Neal',2014);
 insert into m_person (first_name, last_name, entry_year) values ('Ramona','Head',2014);
+
+
+insert into m_consumation (person_id, date, hasConsumed) values (1,'02/04/2015',true);
+insert into m_consumation (person_id, date, hasConsumed) values (2,'04/04/2015',false);
+insert into m_consumation (person_id, date, hasConsumed) values (3,'06/04/2015',true);
+insert into m_consumation (person_id, date, hasConsumed) values (4,'08/04/2015',false);
+-- insert into m_consumation (date_consumation, has_consumed, person_id) values ('2014-04-02',false, 2);
+-- insert into m_consumation (date_consumation, has_consumed, person_id) values ('2014-05-02',true, 3);
+-- insert into m_consumation (date_consumation, has_consumed, person_id) values ('2014-07-02',false, 4);
+
