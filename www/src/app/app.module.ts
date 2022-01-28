a2519c4156e759e0169374492211f22984077e83
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2022-01-28 10:44:32 +0100

    get Consumation Data from backend insert into table

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MealsOverviewComponent } from './view/meals-overview/meals-overview.component';
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
     AppComponent,
     MealsOverviewComponent,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatTableModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
